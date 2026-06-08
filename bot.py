2e5aa1778d1d57ce981f0d344e5c5e62cdff1f6f
VVVV3434 <volodyakwork@gmail.com>
2026-06-08 20:00:00 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,10 @@ import asyncio
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
 
-TOKEN = os.getenv("BOT_TOKEN")
+TOKEN = os.getenv("8867608436:AAGRMJj26VODPBnE0Vte4dAXQ6zVArc73iE")
+
+if not TOKEN:
+    raise RuntimeError("BOT_TOKEN не найден. Проверь Environment Variables в Render.")
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
@@ -11,23 +14,19 @@ dp = Dispatcher()
 
 @dp.message(CommandStart())
 async def start(message: types.Message):
-    await message.answer("Привет! Бот работает 🚀")
+    await message.answer("✅ Это новый чистый бот. Работает только наш код.")
 
 
 @dp.message()
-async def echo(message: types.Message):
-    text = message.text.lower()
-
-    if "привет" in text:
-        await message.answer("И тебе привет!")
-
-    else:
-        await message.answer(f"Ты написал: {message.text}")
+async def any_message(message: types.Message):
+    await message.answer("Я получил сообщение. Всё ок.")
 
 
 async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
     asyncio.run(main())
+ 
